386905976e3d0d001ce0f9e7fb53b820fdf419b9
Brice Nichols <bricenichols@gmail.com>
2024-04-30 12:02:35 -0700

    using OFM data directly from Elmer/ElmerGeo, using psrcelmerpy library

--- aq_scoring/process_data.py
+++ aq_scoring/process_data.py
@@ -1,5 +1,6 @@
 import os, datetime
 import toml
+import psrcelmerpy
 import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
@@ -8,39 +9,36 @@ from util import load_h5
 config = toml.load("configuration.toml")
 start_time = datetime.datetime.now()
 
-# Load parcel data as Pandas DataFrame
-parcel_df = pd.read_csv(config["parcel_file_dir"], sep=r'\s+')
+# Load OFM parcel population from Elmer
+e_conn = psrcelmerpy.ElmerConn()
+ofm_df = e_conn.get_query("select parcel_id, household_pop from ofm.parcelized_saep("+ \
+                                str(config['ofm_vintage'])+", "+str(config['ofm_estimate_year'])+")")
 
-# Create GeoDataFrame from parcel data using XY coordinate values (in WA State Plane coordinates)
-parcel_gdf = gpd.GeoDataFrame(
-    parcel_df,
-    geometry=gpd.points_from_xy(
-        parcel_df['xcoord_p'],
-        parcel_df['ycoord_p'],
-        crs=config["WA_STATE_PLANE_CRS"]
-    ),
-)
+# Load Parcel GeoDataFrame form ElmerGeo
+eg_conn = psrcelmerpy.ElmerGeoConn()
+parcel_gdf = eg_conn.read_geolayer('parcels_urbansim_2018_pts')
+# Convert to WA State Plane Coordinate Reference System
+parcel_gdf = parcel_gdf.to_crs(config["WA_STATE_PLANE_CRS"])
 
-# Load synthetic population from Soundcast to get parcel-level population
-hh_df = load_h5(config['population_file'], 'Household')
-# Total population is sum of household size for all households at each parcel
-df = hh_df.groupby('hhparcel').sum()[['hhsize']].reset_index()
-# Merge population totals to parcels gdf and rename hhsize to represent the transformation (to population)
-parcel_gdf = parcel_gdf.merge(df, left_on='parcelid', right_on='hhparcel', how='left')
-parcel_gdf.rename(columns={'hhsize': 'population'}, inplace=True)
+# Merge OFM data to geodata
+parcel_gdf = parcel_gdf.merge(ofm_df, on='parcel_id', how='left')
 
-# Load the project shapefile
+# Load project shapefile
 project_gdf = gpd.read_file(config['project_dir'])
 project_gdf.crs = config["WA_STATE_PLANE_CRS"]
 
-# Buffer the shapefile geometry
+# Buffer  shapefile geometry
 project_gdf.geometry = project_gdf.buffer(int(config['buffer_distance']))
 
-# Intersect the buffered area with the parcel geodataframe
+# Intersect buffered area with parcel geodataframe
 gdf = gpd.sjoin(project_gdf, parcel_gdf)
 
-# Aggregate the intersection to get population totals
-result_df = gdf[['Fullname','population']].groupby('Fullname').sum()['population']
+# Aggregate intersection to get population totals for each project
+result_df = gdf[['projID','App_ID','household_pop']].groupby(['projID','App_ID']).sum()[['household_pop']].reset_index()
+result_df.rename(columns={
+    'household_pop': f'population_in_{config['buffer_distance']}_ft_buffer'},
+    inplace=True
+    )
 
 if not os.path.exists(config['output_dir']):
     os.makedirs(config['output_dir'])
